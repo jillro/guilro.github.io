6c8fde6aad6db12d7ebb3462a341d1491487849e
Guillaume Royer <guilro@redado.org>
2014-01-20 22:02:21 +0100

    script to add links

---
+++ addlink.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if  [ $# -ne 2 ]
+then
+	echo "Usage : addlink.sh href title";
+	exit;
+fi
+
+sed "s/\
+        <\/ul>/\
+            <li><a href=\"$1\">$2<\/a><\/li>\n\
+        <\/ul>/" links.html >links.html.tmp
+mv links.html.tmp links.html
