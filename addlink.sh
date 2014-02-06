1120c8f204e98fa40cc759d52db904a72b69b477
Guillaume Royer <guilro@redado.org>
2014-02-06 17:50:56 +0100

    update addlink.sh script

--- addlink.sh
+++ addlink.sh
@@ -6,16 +6,9 @@ then
 	exit;
 fi
 
-href=`echo $1 | sed -e 's/[\\/&]/\\\\&/g'`
-title=`echo $2 | sed -e 's/[\\/&]/\\\\&/g'`
+echo "" >> _data/links.yaml
+echo "- href: $1" >> _data/links.yaml
+echo "  title: $2" >> _data/links.yaml
 
-echo $href
-echo $title
-
-sed "s/\
-        <\/ul>/\
-            <li><a href=\"$href\">$title<\/a><\/li>\n\
-        <\/ul>/" links.html >links.html.tmp
-mv links.html.tmp links.html
-git add links.html
-git commit -m "add link $1 $2" links.html
+git add _data/links.yaml
+git commit -m "add link $1 $2" _data/links.yaml
